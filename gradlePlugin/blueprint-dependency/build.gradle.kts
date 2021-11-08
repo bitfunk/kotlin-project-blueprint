4f09d8716441019740babf91fb77706652782c27
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2021-11-08 09:58:13 +0100

    Bump Android Plugin 7.0.3 -> 7.1.0-beta02, Compose 1.0.5 -> 1.1.0-beta02, Compose Material 1.0.5 -> 1.1.0-beta02, Gradle Plugin Dependency Versions 0.38.0 -> 0.39.0 Remove unused versions and cleanup

--- gradlePlugin/blueprint-dependency/build.gradle.kts
+++ gradlePlugin/blueprint-dependency/build.gradle.kts
@@ -25,5 +25,5 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
 }
